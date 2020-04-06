e420163ec69888d87a367eba93e82a30cd4c3520
Trevor <trevorjschmidt97@gmail.com>
2020-04-06 15:04:38 -0600

    First commit

---
+++ Main Lab 1 - Do/main.cpp
@@ -0,0 +1,23 @@
+/*
+Trevor Schmidt, Section 002, trevorjschmidt97@gmail.com
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "\t\t\t\t" << "Basic BYU Trivia" << endl;
+	cout << endl;
+	cout << "\t\t" << "Questions" << "\t\t\t\t" << "Answers" << endl;
+	cout << endl;
+	cout << "What was the original name of BYU?" << "\t\t" << "Brigham Young Academy" << endl;
+	cout << "When was BYU established?" << "\t\t\t" << "1875" << endl;
+	cout << "Who was the first \"permanent\" principal of BYA?" << "\t" << "Karl G. Maeser" << endl;
+	cout << "When did BYA become BYU?" << "\t\t\t" << "1903" << endl;
+	cout << "To what sports conference do we belong?" << "\t\t" << "Independent \\Football\\" << endl;
+	cout << "When did BYU \"win\" the national football title?" << "\t" << "1984" << endl;
+	cout << "Who won the Heisman Trophy in 1990?" << "\t\t" << "Ty Detmer" << endl;
+
+	return 0;
+}
